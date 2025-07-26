96f9a84d3aabe581573b444a6acc5305587444ca
ifounas <founas.ilyas@gmail.com>
2025-07-26 18:15:51 +0200

    ✨ ex05 operator overloading is now understood

--- ex00/srcs/class/Fixed.cpp
+++ ex00/srcs/class/Fixed.cpp
@@ -1,1 +1,33 @@
 #include "Fixed.hpp"
+
+Fixed::Fixed(){
+    std::cout << "Default constructor called" << std::endl;
+    this->fixed = 0;
+}
+
+Fixed::Fixed(int nb)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    this->fixed = nb;
+}
+
+Fixed::~Fixed()
+{
+    std::cout << "Destructor called" << std::endl;
+}
+
+int Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+    // todo
+}
+
+Fixed& Fixed::operator=(const Fixed &other) {
+    if (&other != this) 
+        this->fixed = other.fixed;
+    return *this;
+}
